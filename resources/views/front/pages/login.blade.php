55ff966db22d3da513ae69f151739b326601ca2f
constant <constantkwasigah@gmail.com>
2021-08-18 15:17:30 +0100

    Reg Form modified

--- resources/views/front/pages/login.blade.php
+++ resources/views/front/pages/login.blade.php
@@ -3,26 +3,7 @@
 
 @section('content')
     <!--? slider Area Start-->
-    <div class="slider-area ">
-        <div class="single-slider hero-overly slider-height2 d-flex align-items-center"
-             data-background="{{asset('front/assets/img/hero/about.jpg')}}">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="hero-cap">
-                            <h2>Login</h2>
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="{{route('front.welcome')}}">Home</a></li>
-                                    <li class="breadcrumb-item"><a href="{{route('front.login')}}">Login</a></li>
-                                </ol>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <
     <!-- slider Area End-->
     
                                 <div class="gmnoprint" controlwidth="40" controlheight="20"
--- resources/views/front/pages/register.blade.php
+++ resources/views/front/pages/register.blade.php
@@ -2,109 +2,212 @@
 
 
 @section('content')
-    <!--? slider Area Start-->
-     <div class="slider-area ">
-        <div class="single-slider hero-overly slider-height2 d-flex align-items-center"
-             data-background="{{asset('front/assets/img/hero/about.jpg')}}">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="hero-cap">
-                            <h2>Register</h2>
-                            <nav aria-label="breadcrumb">
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="{{route('front.welcome')}}">Home</a></li>
-                                    <li class="breadcrumb-item"><a href="{{route('front.register')}}">Register</a></li>
-                                </ol>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- slider Area End-->
-    
-                                <div class="gmnoprint" controlwidth="40" controlheight="20"
-                                     style="display: none; position: absolute;">
-                                    <div style="width: 40px; height: 10px;">
-                                        <button draggable="false" title="Rotate map 90 degrees"
-                                                aria-label="Rotate map 90 degrees" type="button"
-                                                class="gm-control-active"
-                                                style="background: none rgb(255, 255, 255); display: none; border: 0px; margin: 0px 0px 32px; padding: 0px; position: relative; cursor: pointer; user-select: none; width: 40px; height: 10px; top: 0px; left: 0px; overflow: hidden; box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px; border-radius: 2px;">
-                                            <img src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2222%22%20viewBox%3D%220%200%2024%2022%22%3E%0A%20%20%3Cpath%20fill%3D%22%23666%22%20fill-rule%3D%22evenodd%22%20d%3D%22M20%2010c0-5.52-4.48-10-10-10s-10%204.48-10%2010v5h5v-5c0-2.76%202.24-5%205-5s5%202.24%205%205v5h-4l6.5%207%206.5-7h-4v-5z%22%20clip-rule%3D%22evenodd%22%2F%3E%0A%3C%2Fsvg%3E%0A"
-                                                 style="height: 10px; width: 18px;"><img
-                                                    src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2222%22%20viewBox%3D%220%200%2024%2022%22%3E%0A%20%20%3Cpath%20fill%3D%22%23333%22%20fill-rule%3D%22evenodd%22%20d%3D%22M20%2010c0-5.52-4.48-10-10-10s-10%204.48-10%2010v5h5v-5c0-2.76%202.24-5%205-5s5%202.24%205%205v5h-4l6.5%207%206.5-7h-4v-5z%22%20clip-rule%3D%22evenodd%22%2F%3E%0A%3C%2Fsvg%3E%0A"
-                                                    style="height: 18px; width: 18px;"><img
-                                                    src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2224%22%20height%3D%2222%22%20viewBox%3D%220%200%2024%2022%22%3E%0A%20%20%3Cpath%20fill%3D%22%23111%22%20fill-rule%3D%22evenodd%22%20d%3D%22M20%2010c0-5.52-4.48-10-10-10s-10%204.48-10%2010v5h5v-5c0-2.76%202.24-5%205-5s5%202.24%205%205v5h-4l6.5%207%206.5-7h-4v-5z%22%20clip-rule%3D%22evenodd%22%2F%3E%0A%3C%2Fsvg%3E%0A"
-                                                    style="height: 18px; width: 18px;"></button>
-                                        <button draggable="false" title="Tilt map" aria-label="Tilt map" type="button"
-                                                class="gm-tilt gm-control-active"
-                                                style="background: none rgb(255, 255, 255); display: block; border: 0px; margin: 0px; padding: 0px; position: relative; cursor: pointer; user-select: none; width: 40px; height: 20px; top: 0px; left: 0px; overflow: hidden; box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px; border-radius: 2px;">
-                                            <img src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2218px%22%20height%3D%2216px%22%20viewBox%3D%220%200%2018%2016%22%3E%0A%20%20%3Cpath%20fill%3D%22%23666%22%20d%3D%22M0%2C16h8V9H0V16z%20M10%2C16h8V9h-8V16z%20M0%2C7h8V0H0V7z%20M10%2C0v7h8V0H10z%22%2F%3E%0A%3C%2Fsvg%3E%0A"
-                                                 style="width: 18px;"><img
-                                                    src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2218px%22%20height%3D%2216px%22%20viewBox%3D%220%200%2018%2016%22%3E%0A%20%20%3Cpath%20fill%3D%22%23333%22%20d%3D%22M0%2C16h8V9H0V16z%20M10%2C16h8V9h-8V16z%20M0%2C7h8V0H0V7z%20M10%2C0v7h8V0H10z%22%2F%3E%0A%3C%2Fsvg%3E%0A"
-                                                    style="width: 18px;"><img
-                                                    src="data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2218px%22%20height%3D%2216px%22%20viewBox%3D%220%200%2018%2016%22%3E%0A%20%20%3Cpath%20fill%3D%22%23111%22%20d%3D%22M0%2C16h8V9H0V16z%20M10%2C16h8V9h-8V16z%20M0%2C7h8V0H0V7z%20M10%2C0v7h8V0H10z%22%2F%3E%0A%3C%2Fsvg%3E%0A"
-                                                    style="width: 18px;"></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                
-  <section class="vh-100 gradient-custom">
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-dark text-white" style="border-radius: 1rem;">
-          <div class="card-body p-5 text-center">
+
+<header>
+ <meta https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css>
+  <meta https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js>
+  <meta https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css>
+  <meta https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js>
+
+
+</header>
+    <style type="text/css">
+
+body {
+    background: #eee
+}
+
+#regForm {
+    background-color: #ffffff;
+    margin: 0px auto;
+    font-family: Raleway;
+    padding: 40px;
+    border-radius: 10px
+}
+
+h1 {
+    text-align: center
+}
+
+input {
+    padding: 10px;
+    width: 100%;
+    font-size: 17px;
+    font-family: Raleway;
+    border: 1px solid #aaaaaa
+}
+
+input.invalid {
+    background-color: #ffdddd
+}
+
+.tab {
+    display: none
+}
+
+button {
+    background-color: #4CAF50;
+    color: #ffffff;
+    border: none;
+    padding: 10px 20px;
+    font-size: 17px;
+    font-family: Raleway;
+    cursor: pointer
+}
+
+button:hover {
+    opacity: 0.8
+}
+
+#prevBtn {
+    background-color: #bbbbbb
+}
+
+.step {
+    height: 15px;
+    width: 15px;
+    margin: 0 2px;
+    background-color: #bbbbbb;
+    border: none;
+    border-radius: 50%;
+    display: inline-block;
+    opacity: 0.5
+}
+
+.step.active {
+    opacity: 1
+}
+
+.step.finish {
+    background-color: #4CAF50
+}
+
+.all-steps {
+    text-align: center;
+    margin-top: 30px;
+    margin-bottom: 30px
+}
+
+.thanks-message {
+    display: none
+}
+
+ </style>
+
+ <script type="text/javascript">
+var currentTab = 0;
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+showTab(currentTab);
+
+});
+
+function showTab(n) {
+var x = document.getElementsByClassName("tab");
+x[n].style.display = "block";
+if (n == 0) {
+document.getElementById("prevBtn").style.display = "none";
+} else {
+document.getElementById("prevBtn").style.display = "inline";
+}
+if (n == (x.length - 1)) {
+document.getElementById("nextBtn").innerHTML = "Submit";
+} else {
+document.getElementById("nextBtn").innerHTML = "Next";
+}
+fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+var x = document.getElementsByClassName("tab");
+if (n == 1 && !validateForm()) return false;
+x[currentTab].style.display = "none";
+currentTab = currentTab + n;
+if (currentTab >= x.length) {
+// document.getElementById("regForm").submit();
+// return false;
+//alert("sdf");
+document.getElementById("nextprevious").style.display = "none";
+document.getElementById("all-steps").style.display = "none";
+document.getElementById("register").style.display = "none";
+document.getElementById("text-message").style.display = "block";
+
+
+
+
+}
+showTab(currentTab);
+}
+
+function validateForm() {
+var x, y, i, valid = true;
+x = document.getElementsByClassName("tab");
+y = x[currentTab].getElementsByTagName("input");
+for (i = 0; i < y.length; i++) { if (y[i].value=="" ) { y[i].className +=" invalid" ; valid=false; } } if (valid) { document.getElementsByClassName("step")[currentTab].className +=" finish" ; } return valid; } function fixStepIndicator(n) { var i, x=document.getElementsByClassName("step"); for (i=0; i < x.length; i++) { x[i].className=x[i].className.replace(" active", "" ); } x[n].className +=" active" ; }
+ </script>
+
+
+      
+      <!-- MultiStep Form -->
+ <section class="vh-100 gradient-custom">
+   <div class="container py-5 h-100">
+   <div class="row d-flex justify-content-center align-items-center h-100">
+   
+     <div class="card bg-dark text-white" style="border-radius: 1rem;" >
+
+<div class="card-body p-5 text-center">
 
             <div class="mb-md-5 mt-md-4 pb-5">
+            <form id="regForm">
+                <h1 id="register" style="color:black">Sign Up</h1>
+                <div class="all-steps" id="all-steps"> <span class="step"></span> <span class="step"></span> <span class="step"></span> <span class="step"></span> </div>
+                <div class="tab">
+                    <p><input placeholder="First name..." oninput="this.className = ''" name="fname"></p>
+                    <p><input placeholder="Last name..." oninput="this.className = ''" name="lname"></p>
+                     <p><input placeholder="Other names..." oninput="this.className = ''" name="oname"></p>
+                     <p><input placeholder="Date of Birth..." type="date"  oninput="this.className = ''" name="bdate"></p>
 
-              <h2 class="fw-bold mb-2 text-uppercase" style="color:white"><a href="{{route('front.dashboard')}}">Register</a></h2>
-              <p class="text-white-50 mb-5" style="color:green">Please enter Bio-Data!</p>
+                </div>
+                <div class="tab">
+                    <p><input placeholder="Institution ID" oninput="this.className = ''" name="instid"></p>
+                    <p><input placeholder="Institution Name" oninput="this.className = ''" name="instname"></p>
+                    <p><input placeholder="City" oninput="this.className = ''" name="city"></p>
+                    <p><input placeholder="State / Region" oninput="this.className = ''" name="state"></p>
+                    <p><input placeholder="Country" oninput="this.className = ''" name="country"></p>
+                </div>
+                <div class="tab">
+                    <p><input placeholder="E-mail..." oninput="this.className = ''" name="email"></p>
+                    <p><input placeholder="Phone..." oninput="this.className = ''" name="phone"></p>
+                </div>
+                <div class="tab">
 
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                <label class="form-label" for="typeEmailX">First Name </label>
-              </div>
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                <label class="form-label" for="typeEmailX">Middle Name</label>
-              </div>
-              <div class="form-outline form-white mb-4">
-                <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                <label class="form-label" for="typeEmailX">Last Name</label>
-              </div>
-              <span class="input-group-addon" style="color:black">
-               <select>
-                 <option>Select Gender</option>
-                 <option>Male</option>
-                 <option>Female</option>
-                 <option>Other</option>
-               </select></span><br><br><br>
+                    <p><input placeholder="Username..." oninput="this.className = ''" name="uname"></p>
+                    <p><input placeholder="Password..." oninput="this.className = ''" name="pword" type="password"></p>
+                    <p><input placeholder="Confirm Password..." oninput="this.className = ''" name="pword" type="password"></p>
+                </div>
+                <div class="thanks-message text-center" id="text-message"> <img src="ok.jpg" width="100" class="mb-4">
+                    <h3>Thanks for signing up! <br>Your information has been saved!<br> We will contact you shortly!</h3> 
+                </div>
+               
+                <div style="overflow:auto;" id="nextprevious">
+                    <div style="float:right;"> <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button> <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button> </div>
+                </div>
 
-              <button class="btn btn-outline-light btn-lg px-5" type="submit"><a href="{{route('front.register2')}}">Next</a></button>
-
-              <div class="d-flex justify-content-center text-center mt-4 pt-1">
+            </form>
+            <div class="d-flex justify-content-center text-center mt-4 pt-1">
                 <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
                 <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
                 <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
               </div>
-
-            </div>
-
-        <form>
-
-
-          </div>
+              </div>
+              </div>
         </div>
-      </div>
     </div>
-  </div>  
+</div>
+</div>
+</div>
+</div>
+
 </section>
-        <!-- ================ registration section end ================= -->
-
-
 @stop
